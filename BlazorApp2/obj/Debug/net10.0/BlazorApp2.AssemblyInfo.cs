c6e0fb924dd166ccd53f26ca9d8c9726fa847efb
Jaedin Hernandez-Rogers <jaedinihernandez@gmail.com>
2026-07-02 13:34:34 -0500

    Commit

--- BlazorApp2/obj/Debug/net10.0/BlazorApp2.AssemblyInfo.cs
+++ BlazorApp2/obj/Debug/net10.0/BlazorApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7a59d8f8dcfb43d1b93d5670bb4e5c52a4c1c14")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
