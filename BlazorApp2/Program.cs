6df86a748add832dad7ed59e94ca3ace8c29cc9e
Jaedin Hernandez-Rogers <jaedinihernandez@gmail.com>
2026-07-12 17:33:47 -0500

    API-KEY-SECRET

--- BlazorApp2/Program.cs
+++ BlazorApp2/Program.cs
@@ -3,6 +3,11 @@ using OddsViewerApp.Services;
 using MudBlazor.Services;
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddEnvironmentVariables();
+
+var secretApiKey = builder.Configuration["THE_ODDS_API_KEY"]
+    ?? builder.Configuration["OddsApi__ApiKey"];
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
@@ -11,8 +16,15 @@ builder.Services.AddMudServices();
 
 
 
-builder.Services.Configure<OddsApiOptions>(
-    builder.Configuration.GetSection("OddsApi"));
+builder.Services.Configure<OddsApiOptions>(options =>
+{
+    builder.Configuration.GetSection("OddsApi").Bind(options);
+
+    if (string.IsNullOrWhiteSpace(options.ApiKey) && !string.IsNullOrWhiteSpace(secretApiKey))
+    {
+        options.ApiKey = secretApiKey;
+    }
+});
 
 builder.Services.AddHttpClient<OddsApiService>((serviceProvider, client) =>
 {
